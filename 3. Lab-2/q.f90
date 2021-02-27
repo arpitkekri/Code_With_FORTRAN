754b1ca0b78193410cd8122169b64754cddc9c6e
arpitkekri <jainarpitkekri@gmail.com>
2021-02-27 23:52:50 +0530

    adding output file

--- 3. Lab-2/q.f90
+++ 3. Lab-2/q.f90
@@ -4,7 +4,6 @@ PROGRAM  Arpit
 
     REAL, PARAMETER :: pi = acos(-1.0)
     REAL:: x, x0, delta, k0, u, Etrans, Gx
-    CHARACTER*10:: filename
     INTEGER:: i, outunit
     x0 = 10.0
     delta = 0.25
@@ -13,8 +12,7 @@ PROGRAM  Arpit
 
     k0 = sqrt(2 * u * Etrans - (1./(2 * delta * delta)))
 
-    WRITE(filename, fmt = "(a, i0)") "quiz1.", 1
-    OPEN(UNIT = outunit, FILE = filename, FORM = "formatted")
+    OPEN(UNIT = outunit, FILE = 'quiz1.txt', FORM = "formatted")
     x = 0.35                    ! Starting point is 0.5 so in first iteration 0.35 + 0.15 = 0.5
     DO i = 1, 128               ! Grid points = 128
         x = x + 0.15            ! With dexX = 0.15  
