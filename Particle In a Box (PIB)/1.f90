e3244beb3dd89f3848537cb7a2e88892037c2164
arpitkekri <jainarpitkekri@gmail.com>
2021-03-10 03:23:57 +0530

    PIB1D_INFINITE_WELL Done

--- Particle In a Box (PIB)/1.f90
+++ Particle In a Box (PIB)/1.f90
@@ -1,12 +1,12 @@
 PROGRAM PIB1D_INFINITE_WELL
     IMPLICIT NONE
 
-    REAL, ALLOCATABLE:: pot(:), hmat(:,:), diag(:), eigvec(:,:), x(:), work(:) 
     REAL, PARAMETER :: pi = acos(-1.0)
-    REAL:: xmax, xmin, dx, hbar = 1.0, m = 1.0, tconst, tmp, xexpect
-    INTEGER:: nx, lwork, i, j, ifail, nexpect
-
-    integer n
+    REAL, ALLOCATABLE:: pot(:), x(:)
+    DOUBLE PRECISION, ALLOCATABLE:: hmat(:,:), work(:), diag(:)
+    DOUBLE PRECISION:: xexpect
+    REAL:: xmax, xmin, dx, hbar = 1.0, m = 1.0, tconst
+    INTEGER:: nx, lwork, i, ifail, nexpect, n
 
     WRITE(*, fmt = '(/A)', ADVANCE = 'NO') "Enter the number of grid points: "
     READ(*, *) nx
@@ -15,61 +15,53 @@ PROGRAM PIB1D_INFINITE_WELL
     xmin = 0.0
     dx = (xmax - xmin)/(nx - 1)
     
-    ALLOCATE(pot(nx), hmat(nx,nx), diag(nx), eigvec(nx,nx), x(nx), work(64*nx))
+    ALLOCATE(pot(nx), hmat(nx,nx), diag(nx), x(nx), work(64*nx))
     DO i = 1, nx
         x(i) = xmin + (i-1)*dx
     ENDDO
 
     tconst = (hbar**2) / (2.0 * m * dx**2)
     hmat = 0.0
-    pot = 0.0
-    DO i = 1, nx
-        hmat(i, i) = pot(i) + 2.0 * tconst 
-    ENDDO
 
-    DO i = 1, nx
-        DO j = i+1, nx 
-            IF(abs(i-j) .EQ. 1) hmat(i, j) = -tconst 
-        ENDDO 
-        DO j = i-1, nx 
-            IF(abs(i-j) .EQ. 1) hmat(i, j) = -tconst
-        ENDDO 
+    pot = 0.0
+
+    DO i = 1, nx-1
+        hmat(i, i) = pot(i) + 2.0 * tconst
+        hmat(i, i+1) = -tconst
+        hmat(i+1, i) = -tconst
     ENDDO
-    
-    DO i = 1, nx 
-        ! WRITE(*, "(100f10.6)")(hmat(i, j), j = 1, i)
-    ENDDO  
+    hmat(nx, nx) = pot(nx) + 2.0 * tconst
     
     lwork = 64*nx
-    CALL dsyev('v', 'u', nx, hmat, nx, diag, work, lwork, ifail)
+    CALL dsyev('V', 'U', nx, hmat, nx, diag, work, lwork, ifail)
     
-    DO i = 1, nx
-        DO j = i, nx
-            IF(diag(i) .GT. diag(j)) THEN
-                tmp = diag(j)
-                diag(j) = diag(i)
-                diag(i) = tmp
-            ENDIF 
-        ENDDO
-    ENDDO
-    
-    OPEN(UNIT = 20, FILE = 'output.txt')
-        WRITE(20, *) '# Results'
-        WRITE(20, *) '#n    E1  Eexact  wf'
+    ! Sorting but no need as diag is already sorted
+    ! DO i = 1, nx
+    !     DO j = i, nx
+    !         IF(diag(i) .GT. diag(j)) THEN
+    !             tmp = diag(j)
+    !             diag(j) = diag(i)
+    !             diag(i) = tmp
+    !         ENDIF 
+    !     ENDDO
+    ! ENDDO
 
-        DO i = 1, 5
+    OPEN(UNIT = 20, FILE = 'output.txt')
+        ! WRITE(20, *) '#x    E1  Eexact    WaveFunction'
+        DO i = 1, nx
             WRITE(20, 10) x(i), diag(i), i**2 * pi**2/(2.0 * (xmax-xmin)**2), (hmat(i, n)/sqrt(dx), n = 1, 5)
-            10 FORMAT(f8.3, 3x, f8.3, 3x, f8.3, 3x, f8.3, 3x, f8.3, 3x, f8.3, 3x, f8.3, 3x, f8.3, 3x)
-        ENDDO 
+            10 FORMAT(f8.3, 3x, f0.3, 3x, f0.3, 3x, f8.3, 3x, f8.3, 3x, f8.3, 3x, f8.3, 3x, f8.3, 3x)
+        ENDDO
     CLOSE(20)
     
-    PRINT *, "Expectation value of x for n = ?"
+    WRITE(*, fmt = '(/A)', ADVANCE = 'NO') "Expectation value for n = "
     READ(*, *) nexpect
     xexpect = 0.0
     
     DO i = 1, nx 
         xexpect = xexpect + hmat(i, nexpect) * x(i) * hmat(i, nexpect) !*dx
     ENDDO
-    PRINT *, xexpect
+
+    WRITE(*, *) xexpect
 
 END PROGRAM PIB1D_INFINITE_WELL
