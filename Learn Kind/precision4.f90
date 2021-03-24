059a89df54967b180df004103e20bfbd5ce19d91
arpitkekri <jainarpitkekri@gmail.com>
2021-03-24 14:10:36 +0530

    Truncate numbers without use of KIND

--- Learn Kind/precision4.f90
+++ Learn Kind/precision4.f90
@@ -1,8 +1,15 @@
-program test
-implicit none
-REAL :: X, y, z, x1, y1, z1, x2, y2, z2
+PROGRAM truncate_without_kind
+    IMPLICIT NONE
 
-x=0.0;y=7.0;z=0.25;x1=1.23;y1=1.23E12;z1=0.1;x2=1.0E-1;y2=3.141592653589793
+    REAL:: x, y, z, x1, y1, z1, x2, y2
+    x = 0.0
+    y = 7.0
+    z = 0.25
+    x1 = 1.23
+    y1 = 1.23E12
+    z1 = 0.1
+    x2 = 1.0E-1
+    y2 = 3.141592653589793
+    PRINT *, x, y, z, x1, y1, z1, x2, y2, kind(x1)
 
-PRINT* , x,y,z,x1,y1,z1,x2,y2,kind(x1)
-END
+END PROGRAM truncate_without_kind
