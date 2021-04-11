f8b5baa55c860ece3dc1de4de82d8671470b1662
arpitkekri <jainarpitkekri@gmail.com>
2021-04-11 22:41:19 +0530

    Code for chemical kinetics problem (All 3 methods)

--- ODEs/Chemical Kinetics.f90
+++ ODEs/Chemical Kinetics.f90
@@ -11,16 +11,16 @@ MODULE RxnConstant
     INTEGER, PARAMETER:: n = 4
     REAL(KIND = dp), PARAMETER:: kab = 1, kba = 3, kbc = 4.2, kcb = 7.3, kcd = 0.4
 ENDMODULE RxnConstant
-  
+
 PROGRAM Chemical_Kinetics_Rxn
     USE precision
     USE RxnConstant
     IMPLICIT NONE
-  
+
     REAL(KIND = dp):: concentration(n)
     REAL(KIND = dp):: time, dt, tmax
     INTEGER:: nt, i
-    
+
     time = 0;
     dt = 0.0001;
     tmax = 60;
@@ -32,24 +32,56 @@ PROGRAM Chemical_Kinetics_Rxn
     OPEN(unit = 10, file = "output.txt")
     DO i = 1, nt
         time = time + dt;
-        CALL Euler(concentration, dt, Func(concentration))
+        ! CALL Euler(concentration, dt)
+        ! CALL RK2(concentration, dt)
+        CALL RK4(concentration, dt)
         WRITE(10, *) time, concentration
     ENDDO
     CLOSE(10)
 
     CONTAINS
-    SUBROUTINE Euler(concentration, dt, Func)
+    SUBROUTINE Euler(concentration, dt)
         USE precision
+        USE RxnConstant
         IMPLICIT NONE
-        
+
         REAL(KIND = dp):: concentration(n), Func(n)
         REAL(KIND = dp):: dt
-        
+
         ! Euler Propagation
+        CALL DbyDT(concentration, Func)
         concentration = concentration + dt * Func
     END SUBROUTINE Euler
 
-    FUNCTION Func(concentration)
+    SUBROUTINE RK2(concentration, dt)
+        USE precision
+        IMPLICIT NONE
+
+        REAL(KIND = dp):: concentration(n), sl(n), sr(n)
+        REAL(KIND = dp):: dt
+
+        ! RK2 Propagation
+        CALL DbyDT(concentration, sl)
+        CALL DbyDT(concentration + dt * sl, sr)
+        concentration = concentration + dt * (sl + sr)/2.0_dp
+    END SUBROUTINE RK2
+
+    SUBROUTINE RK4(concentration, dt)
+        USE precision
+        IMPLICIT NONE
+
+        REAL(KIND = dp):: concentration(n), s1(n), s2(n), s3(n), s4(n)
+        REAL(KIND = dp):: dt
+
+        ! RK2 Propagation
+        CALL DbyDT(concentration, s1)
+        CALL DbyDT(concentration + dt * s1/2.0_dp, s2)
+        CALL DbyDT(concentration + dt * s2/2.0_dp, s3)
+        CALL DbyDT(concentration + dt * s3, s4)
+        concentration = concentration + dt * (s1 + 2*s2 + 2*s3 + s4)/6.0_dp
+    END SUBROUTINE RK4
+
+    SUBROUTINE DbyDT(concentration, Func)
         USE precision
         USE RxnConstant
         IMPLICIT NONE
@@ -60,6 +92,6 @@ PROGRAM Chemical_Kinetics_Rxn
         Func(2) = kab * concentration(1) - kba * concentration(2) - kbc * concentration(2) + kcb * concentration(3)**2
         Func(3) = 2 * kbc * concentration(2) - 2 * kcb * concentration(3)**2 - kcd * concentration(3)
         Func(4) = kcd * concentration(3)
-    END FUNCTION Func
+    END SUBROUTINE DbyDT
 
 END PROGRAM Chemical_Kinetics_Rxn
